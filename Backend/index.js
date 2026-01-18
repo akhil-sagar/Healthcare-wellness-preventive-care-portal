332d389f8f3bec6a758e0fe6e0bc37ada09a37aa
akhil-sagar <nani5202220@gmail.com>
2026-01-18 14:04:49 +0530

    changes in index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,18 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRouter from './routers/auth.router.js'
+import bodyParser from 'body-parser';
+dotenv.config();
 const app=express();
 
+app.get("/", (req, res) => {
+  res.send("Hello from GET request");
+});
 
+app.use(bodyParser.urlencoded())
 
-
-app.use((err ,req ,res ,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || "Internal server error !";
-    return res.status(500).json({
-        success: false,
-        statusCode,
-        message,
-    });
-})
-
+app.use(bodyParser.json())
 app.listen(3000,()=>{
     console.log("server is running at port 3000");
 })
